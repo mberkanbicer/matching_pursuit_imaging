c5043153d48698accd8a5ac7f6dca6aa4c2a7194
Mustafa Berkan BİÇER <mberkanbicer@gmail.com>
2021-06-21 01:43:43 +0300

    indentation

--- matplot2.m
+++ matplot2.m
@@ -4,8 +4,8 @@
 %             F: Frequency vector
 %             T: Time Vector
 function [pp, p]=matplot2(T,F,x,r)
-b=max(max(abs(x)));
-ra=b/(10^(r/20));
-p=x.*(abs(x)>=ra)+ra*ones(size(x)).*(abs(x)<ra);
-pp=20*log10(abs(p)/b);
-colormap(jet(256)),imagesc(T,F,pp);
+	b=max(max(abs(x)));
+	ra=b/(10^(r/20));
+	p=x.*(abs(x)>=ra)+ra*ones(size(x)).*(abs(x)<ra);
+	pp=20*log10(abs(p)/b);
+	colormap(jet(256)),imagesc(T,F,pp);
